50b865a07b15b034f7b0a4da9c0076c41cbad52d
PragyaMaheshwari14 <pragyam1403@gmail.com>
2026-05-16 12:53:42 +0530

    code cleanup across stack

--- backend/src/services/storage.service.js
+++ backend/src/services/storage.service.js
@@ -1,25 +1,3 @@
-// const ImageKit = require("@imagekit/nodejs");
-
-// const imagekit = new ImageKit({
-//     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-//     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-//     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-// });
-
-// async function uploadFile(file, fileName) {
-
-//     const result = await imagekit.files.upload({
-//         file: file,
-//         fileName: fileName,
-//     });
-
-//     return result;
-// }
-
-// module.exports = {
-//     uploadFile
-// }
-
 const ImageKit = require("@imagekit/nodejs");
 
 const imagekit = new ImageKit({
